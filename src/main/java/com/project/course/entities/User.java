461ff1fb3fd9244c94819fcf16b1b72b9288cb28
Rafael <rafaelsdias1@gmail.com>
2025-07-07 15:24:24 +0100

    H2 database, test profile, JPA

--- src/main/java/com/project/course/entities/User.java
+++ src/main/java/com/project/course/entities/User.java
@@ -1,14 +1,19 @@
 package com.project.course.entities;
 
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serializable;
 
 
 @Data
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
