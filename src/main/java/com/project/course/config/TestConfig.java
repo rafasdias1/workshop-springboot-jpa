c5b17619d58c6852cec99a42ee6efa5cc9ade85f
Rafael <rafaelsdias1@gmail.com>
2025-07-08 14:37:44 +0100

    Category

--- src/main/java/com/project/course/config/TestConfig.java
+++ src/main/java/com/project/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.project.course.config;
 
+import com.project.course.entities.Category;
 import com.project.course.entities.Order;
 import com.project.course.entities.User;
 import com.project.course.enums.OrderStatus;
+import com.project.course.repositories.CategoryRepository;
 import com.project.course.repositories.OrderRepository;
 import com.project.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -34,9 +39,16 @@ public class TestConfig implements CommandLineRunner {
         Order o2=new Order(null, Instant.parse("2019-06-21T15:21:01Z"),OrderStatus.WAITING_PAYMENT,u2);
         Order o3=new Order(null, Instant.parse("2019-07-22T10:22:21Z"),OrderStatus.WAITING_PAYMENT,u1);
 
+        Category c1=new Category(null,"Eletronics");
+        Category c2=new Category(null,"Books");
+        Category c3=new Category(null,"Computers");
+
+
         userRepository.saveAll(Arrays.asList(u1,u2));
 
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
+        categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
 
     }
 }
---
+++ src/main/java/com/project/course/entities/Category.java
@@ -0,0 +1,25 @@
+package com.project.course.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "tb_category")
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+}
