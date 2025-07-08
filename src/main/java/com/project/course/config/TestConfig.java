34c6a503364f6ed6bc7cb256ccb7786a088c4b3b
Rafael <rafaelsdias1@gmail.com>
2025-07-08 11:57:06 +0100

    OrderStatus enum

--- src/main/java/com/project/course/config/TestConfig.java
+++ src/main/java/com/project/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.project.course.config;
 
 import com.project.course.entities.Order;
 import com.project.course.entities.User;
+import com.project.course.enums.OrderStatus;
 import com.project.course.repositories.OrderRepository;
 import com.project.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1= new User(null,"Maria Brown","maria@gmail.com","963271973","12345");
         User u2= new User(null,"Alex Green","alex@gmail.com","963316173","12431");
 
-        Order o1=new Order(null, Instant.parse("2019-06-20T19:53:07Z"),u1);
-        Order o2=new Order(null, Instant.parse("2019-06-21T15:21:01Z"),u2);
-        Order o3=new Order(null, Instant.parse("2019-07-22T10:22:21Z"),u1);
+        Order o1=new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,u1);
+        Order o2=new Order(null, Instant.parse("2019-06-21T15:21:01Z"),OrderStatus.WAITING_PAYMENT,u2);
+        Order o3=new Order(null, Instant.parse("2019-07-22T10:22:21Z"),OrderStatus.WAITING_PAYMENT,u1);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
 
--- src/main/java/com/project/course/entities/Order.java
+++ src/main/java/com/project/course/entities/Order.java
@@ -2,6 +2,7 @@ package com.project.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.course.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,9 +26,28 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
 
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus status) {
+        if (status != null) {
+            this.orderStatus = status.getCode();
+        }
+    }
 
 }
